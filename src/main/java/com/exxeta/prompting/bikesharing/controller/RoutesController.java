72970c67aaceb70a5251bf0f7388617581905d66
Leo Helfferich <exxeta.helfferich@extaccount.com>
2024-02-06 11:29:56 +0100

    not implemented responses

--- src/main/java/com/exxeta/prompting/bikesharing/controller/RoutesController.java
+++ src/main/java/com/exxeta/prompting/bikesharing/controller/RoutesController.java
@@ -1,46 +1,61 @@
 package com.exxeta.prompting.bikesharing.controller;
 
+import com.exxeta.prompting.bikesharing.model.Bike;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RestController
 @RequestMapping("api")
 public class RoutesController {
 
     @PostMapping("/register")
-    public void register() {
+    public ResponseEntity<Void> register() {
         // TODO
+        return notImplementedResponse();
     }
 
     @PostMapping("/login")
-    public void login() {
+    public ResponseEntity<Void> login() {
         // TODO
+        return notImplementedResponse();
     }
 
     @PostMapping("/logout")
-    public void logout() {
+    public ResponseEntity<Void> logout() {
         // TODO
+        return notImplementedResponse();
     }
 
     @PostMapping("/rent/start")
-    public void startRent() {
+    public ResponseEntity<Void> startRent() {
         // TODO
+        return notImplementedResponse();
     }
 
     @PostMapping("/rent/stop")
-    public void stopRent() {
+    public ResponseEntity<Void> stopRent() {
         // TODO
+        return notImplementedResponse();
     }
 
-    @PostMapping("/nearest-bikes")
-    public void nearestBikes() {
+    @GetMapping("/nearest-bikes")
+    public ResponseEntity<Collection<Bike>> nearestBikes() {
         // TODO
+        return notImplementedResponse();
     }
 
     @GetMapping("/test")
-    public void test() {
-        // TODO
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("Hello World");
+    }
+
+    private static <T> ResponseEntity<T> notImplementedResponse() {
+        return ResponseEntity.status(HttpStatusCode.valueOf(501)).build();
     }
 }
